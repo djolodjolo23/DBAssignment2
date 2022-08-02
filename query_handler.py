a70109504b71fad5e6f615be0d774cbcce648644
Djordje <dd222im@student.lnu.se>
2022-08-02 12:24:25 +0200

    Adding more queries, added a query for creating a View and fetching the data from it

--- query_handler.py
+++ query_handler.py
@@ -36,8 +36,56 @@ def query2():
 
 # INGREDIENT NAMES FOR A GIVEN RECIPE
 def query3():
-    query = "SELECT NAME from rm_ingredient JOIN recipe_ingredient on rm_ingredient.INGREDIENT_ID = recipe_ingredient.INGREDIENT_ID JOIN rm_recipe on recipe_ingredient.RECIPE_ID = rm_recipe.RECIPE_ID WHERE rm_recipe.RECIPE_NAME = 'Carbonara'"
+    query = """
+            SELECT NAME from rm_ingredient 
+            JOIN recipe_ingredient on rm_ingredient.INGREDIENT_ID = recipe_ingredient.INGREDIENT_ID 
+            JOIN rm_recipe on recipe_ingredient.RECIPE_ID = rm_recipe.RECIPE_ID 
+            WHERE rm_recipe.TITLE = 'Carbonara'
+            """
     mycursor.execute(query)
     for row in mycursor:
         print (row)
 
+def query4():
+    query = """
+            SELECT NAME, rm_recipe.TITLE, rm_recipe.DESCRIPTION
+            FROM rm_useracc
+            JOIN rm_recipe ON rm_useracc.USERACC_ID = rm_recipe.USERACC_ID
+            WHERE rm_useracc.NAME = 'Djordje'
+            """
+    mycursor.execute(query)
+    for row in mycursor:
+        print (row)
+
+def query5():
+    query = """
+            SELECT rm_useracc.NAME, COUNT(TITLE) FROM rm_recipe
+            JOIN rm_useracc on rm_recipe.USERACC_ID = rm_useracc.USERACC_ID
+            GROUP BY rm_useracc.NAME
+            """
+    mycursor.execute(query)
+    for row in mycursor:
+        print (row)
+
+#getting the recipe title with all the ingredients concatenated
+def query6():
+    query = """
+            CREATE VIEW Recipe_View AS
+            SELECT rm_useracc.NAME, rm_recipe.RECIPE_ID, rm_recipe.TITLE, rm_recipe.COOKING_TIME, rm_recipe.DESCRIPTION
+            FROM rm_useracc, rm_recipe
+            WHERE rm_useracc.USERACC_ID = rm_recipe.USERACC_ID
+            ORDER BY rm_useracc.NAME
+            """
+    mycursor.execute(query)
+    for row in mycursor:
+        print (row)
+# selecting everything from Recipe_View
+def query7():
+    query = """
+            SELECT * FROM Recipe_View
+            """
+    mycursor.execute(query)
+    for row in mycursor:
+        print (row)
+
+query7()
